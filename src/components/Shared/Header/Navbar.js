4f0c058cbad1fe4d6e82443ac9c5a56fa3263e4a
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-05-04 00:39:34 +0600

    footer section complete

--- src/components/Shared/Header/Navbar.js
+++ src/components/Shared/Header/Navbar.js
@@ -11,7 +11,6 @@ import Image from 'next/image';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import XIcon from '@mui/icons-material/X';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import { Twitter } from '@mui/icons-material';
 import Stack from '@mui/material/Stack';
