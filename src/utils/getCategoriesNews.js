9078a7e59da05513d5908ec0538a180da4e223dd
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-05-05 22:02:41 +0600

    dynamic categories and card implemented

--- src/utils/getCategoriesNews.js
+++ src/utils/getCategoriesNews.js
@@ -1,4 +1,6 @@
 export const getCategoriesNews = async (category) => {
-    const res = await fetch(`https://the-news-portal-server.vercel.app/news?category=${category}` , {cache:"no-store"})
-    return res.json()
-}
+  const res = await fetch(
+    `https://the-news-portal-server.vercel.app/news?category=${category}`
+  );
+  return res.json();
+};
